ae013bd32bd912cd289a406c5a1f90d2df7fc3c0
HaritsTama <nayotama1818@gmail.com>
2024-06-03 10:38:37 +0700

    menambahkan fungsi main yang berisi 1 try dan 2 catch

---
+++ ManualException/ManualException.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <exception>
+using namespace std;
+
+int main()
+{
+	try
+	{
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		//throw 0.5; //melemparkan sebuat interger maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a)
+	{
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...)
+	{
+		/*jika selain interger maka block ini akan dieksekusi*/
+		cout << "default Pengecualian dieksekusi" << endl;
+	}
+}
