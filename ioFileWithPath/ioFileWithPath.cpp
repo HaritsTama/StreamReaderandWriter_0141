6c260f21745b7e1e9a12d6b503c2ef05396a5819
HaritsTama <nayotama1818@gmail.com>
2024-06-03 10:27:44 +0700

    membuka file nya dalam mode membaca dan menutup filenya

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -35,6 +35,28 @@ int main()
 	//selesai dalam menulis sekarang tutup filenya
 	outfile.close();
 
+	//Membuka file dalam mode membaca
+	ifstream infile;
+
+	//Menunjukkan ke sebuah file
+	infile.open("contohfile.txt", ios::in);
+
+	cout << endl << ">= Membuka dabn membaca file " << endl;
+	//Jika file ada maka
+	if (infile.is_open())
+	{
+		//Melakukkan perulangan setiap baris
+		while (getline(infile, baris))
+		{
+			//dan tampilkan di sini
+			cout << baris << '\n';
+		}
+		//tutup file tersebut setelah selesai
+		infile.close();
+	}
+	//Jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
 
 
 }
