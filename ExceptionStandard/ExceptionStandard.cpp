9c731e625d61d8b92cf5bd4e2c07b5c4b286ee1a
HaritsTama <nayotama1818@gmail.com>
2024-06-03 10:34:40 +0700

    membuat fungsi main dimana berisi try dan catch

--- ExceptionStandard/ExceptionStandard.cpp
+++ ExceptionStandard/ExceptionStandard.cpp
@@ -5,5 +5,16 @@ using namespace std;
 
 int main()
 {
+	cout << "Awal Program" << endl;
+	try
+	{
+		array<int, 3> data = { 1,2,3 };
+		cout << data.at(5) << endl;
+	}
 
+	catch (exception& e)
+	{
+		cout << e.what() << endl;
+	}
+	cout << "Baris program yang terakhir" << endl;
 }
